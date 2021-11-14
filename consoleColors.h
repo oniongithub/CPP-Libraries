75874a71c66e6c2ebac622a87d2e7bfdcfbcb3a9
onion <62121953+oniongithub@users.noreply.github.com>
2021-11-14 18:00:12 -0500

    Update consoleColors.h

--- consoleColors.h
+++ consoleColors.h
@@ -12,28 +12,28 @@ namespace conColors {
 		return scrBuffer;
 	}
 
-	static inline void setScreenBuffer(CONSOLE_SCREEN_BUFFER_INFOEX scrBuffer)
+	static inline void setScreenBuffer(CONSOLE_SCREEN_BUFFER_INFOEX scrBuffer, int attributeInd)
 	{
 		HANDLE conHandle = GetStdHandle(STD_OUTPUT_HANDLE);
 		SetConsoleScreenBufferInfoEx(conHandle, &scrBuffer);
-		SetConsoleTextAttribute(conHandle, 3);
+		SetConsoleTextAttribute(conHandle, attributeInd);
 	}
 
-	static inline void setBackground(int r, int g, int b)
+	static inline void setBackground(int r, int g, int b, int attribute = 0)
 	{
 		auto scrBuffer = getScreenBuffer();
 
 		scrBuffer.ColorTable[0] = RGB(r, g, b);
 
-		setScreenBuffer(scrBuffer);
+		setScreenBuffer(scrBuffer, attribute);
 	}
 
-	static inline void setForeground(int r, int g, int b)
+	static inline void setForeground(int r, int g, int b, int attribute = 0)
 	{
 		auto scrBuffer = getScreenBuffer();
 
 		scrBuffer.ColorTable[3] = RGB(r, g, b);
 
-		setScreenBuffer(scrBuffer);
+		setScreenBuffer(scrBuffer, attribute);
 	}
 };
