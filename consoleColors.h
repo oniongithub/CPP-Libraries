9bbd3f0e66d0bc79eb5edbd7ee30fc7923862110
onion <62121953+oniongithub@users.noreply.github.com>
2021-11-29 13:30:53 -0500

    Update consoleColors.h

--- consoleColors.h
+++ consoleColors.h
@@ -2,7 +2,7 @@
 #include <Windows.h>
 
 namespace conColors {
-	static inline CONSOLE_SCREEN_BUFFER_INFOEX getScreenBuffer()
+	static inline CONSOLE_SCREEN_BUFFER_INFOEX getScreenBuffer() noexcept
 	{
 		CONSOLE_SCREEN_BUFFER_INFOEX scrBuffer;
 		scrBuffer.cbSize = sizeof(CONSOLE_SCREEN_BUFFER_INFOEX);
@@ -12,28 +12,24 @@ namespace conColors {
 		return scrBuffer;
 	}
 
-	static inline void setScreenBuffer(CONSOLE_SCREEN_BUFFER_INFOEX scrBuffer, int attributeInd)
+	static inline void setScreenBuffer(CONSOLE_SCREEN_BUFFER_INFOEX scrBuffer, int attributeInd) noexcept
 	{
 		HANDLE conHandle = GetStdHandle(STD_OUTPUT_HANDLE);
 		SetConsoleScreenBufferInfoEx(conHandle, &scrBuffer);
 		SetConsoleTextAttribute(conHandle, attributeInd);
 	}
 
-	static inline void setBackground(int r, int g, int b, int attribute = 0)
+	static inline void setBackground(int r, int g, int b, int attribute = 0) noexcept
 	{
 		auto scrBuffer = getScreenBuffer();
-
 		scrBuffer.ColorTable[0] = RGB(r, g, b);
-
 		setScreenBuffer(scrBuffer, attribute);
 	}
 
-	static inline void setForeground(int r, int g, int b, int attribute = 0)
+	static inline void setForeground(int r, int g, int b, int attribute = 0) noexcept
 	{
 		auto scrBuffer = getScreenBuffer();
-
 		scrBuffer.ColorTable[3] = RGB(r, g, b);
-
 		setScreenBuffer(scrBuffer, attribute);
 	}
 };
